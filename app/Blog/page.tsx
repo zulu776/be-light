01e0c006ec0304d56065605294bc59f16fa6850d
Sebastian Zuluaga B <zuluaga776@gmail.com>
2026-04-01 17:39:07 -0500

    arreglo paginas sin export

---
+++ app/Blog/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Blog() {
+    return (
+        <div className="w-full">
+            <p className="font-sans text-textPrimary text-2xl py-8">Blog coming soon...</p>
+        </div>
+    )
+}
